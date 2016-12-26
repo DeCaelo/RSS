b46490015eab033f23ebc2dc2a5a9f69a63e38ce
Ludovic Cleuet <ludovic.cleuet@gmail.com>
2016-12-26 09:31:29 +0100

    setState in constructor

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyAqStmlkaNA_KWF9oKFZ21Z22GOgcjRGEc';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
   return (
     <div>
       <SearchBar />
     </div>
     );
+  }
 }
 
 ReactDOM.render(
